a97b380c0bfbd93b2b86e84451c57fa269c4eb8b
Zack Piper <zack@chronosinteractive.com>
2017-02-18 22:30:22 -0800

    add edit toggling

--- src/Components/Property.js
+++ src/Components/Property.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 
 class Property extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      isEditing: false,
+    }
+  }
+
+  toggleIsEditing(isEditing) {
+    this.setState({
+      isEditing,
+    });
+  }
+
   onDelete() {
     this.props.onDelete(this.props.id);
   }
@@ -8,9 +22,26 @@ class Property extends Component {
   render() {
     return (
       <li className="property">
-        <h3>{this.props.title}</h3>
-        <h4>{this.props.url}</h4>
-        <button onClick={this.onDelete.bind(this)}>Delete</button>
+        {
+          this.state.isEditing
+          ?
+          <form>
+            <input type="text"
+                   value={this.props.title} />
+            <input type="url"
+                   value={this.props.url} />
+            <button type="submit">Okey Doke</button>
+            <button type="button"
+                    onClick={this.toggleIsEditing.bind(this, false)}>Cancel</button>
+          </form>
+          :
+          <div>
+            <h3>{this.props.title}</h3>
+            <h4>{this.props.url}</h4>
+            <button onClick={this.toggleIsEditing.bind(this, true)}>Edit</button>
+            <button onClick={this.onDelete.bind(this)}>Delete</button>
+          </div>
+        }
       </li>
     );
   }
