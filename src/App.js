af7a08a1cfe497ad59e7133b364aa39ad42c3e5c
Zack Piper <zack@chronosinteractive.com>
2017-02-18 16:13:45 -0800

    set up axios config

--- src/App.js
+++ src/App.js
@@ -6,7 +6,25 @@ import PropertyList from './Components/PropertyList';
 
 import './App.css';
 
+const baseURL = 'http://clientside-api.herokuapp.com'
+const authKey = 'd0aed402a00bd1c7188373ba8dd20aab';
+
+const instance = axios.create({
+  baseURL: baseURL,
+  headers: { Authorization: authKey }
+});
+
 class App extends Component {
+  componentDidMount() {
+    instance.get('/api/v1/listings')
+      .then((response) => {
+        // this.setState({});
+      })
+      .catch((error) => {
+        console.log('Error fetching and parsing data', error);
+      });
+  }
+
   render() {
     return (
       <div className="app">
