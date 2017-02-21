e56a2273357544c635c68a979de8a64d285674a5
Zack Piper <zack@chronosinteractive.com>
2017-02-20 19:20:57 -0800

    format utils.js

--- src/utilities/utils.js
+++ src/utilities/utils.js
@@ -1,12 +1,12 @@
 const utils = {
   findWithAttr(array, attr, value) {
-    for(var i = 0; i < array.length; i += 1) {
+    for (let i = 0; i < array.length; i += 1) {
       if(array[i][attr] === value) {
         return i;
       }
     }
     return -1;
-  }
+  },
 };
 
 export default utils;
