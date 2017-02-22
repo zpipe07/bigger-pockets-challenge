8aa2579c75e9b932b3de165278469e572d36c832
Zack Piper <zack@chronosinteractive.com>
2017-02-21 22:32:52 -0800

    add jsDocs to utils

--- src/utilities/utils.js
+++ src/utilities/utils.js
@@ -1,4 +1,15 @@
+/**
+ * Utility methods
+ * @type {Object}
+ */
 const utils = {
+  /**
+   * Find the index for the object with the given key-value pair
+   * @param  {array}  array - array of object to search
+   * @param  {string} attr  - attribute to search
+   * @param  {string} value - value of attribute to find
+   * @return {number}       - index of object or -1
+   */
   findWithAttr(array, attr, value) {
     for (let i = 0; i < array.length; i += 1) {
       if(array[i][attr] === value) {
